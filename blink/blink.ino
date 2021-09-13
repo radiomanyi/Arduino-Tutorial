0e0e80be16b85fe1353abdc53767181e9c121bf2
radiomanyi <pyzyf1983@gmail.com>
2021-09-13 09:16:31 +0800

    First Upload
    Demo of Pin 13 blinking effect with 500ms interval.

---
+++ blink/blink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);//设置11号引脚为输出模式
+  pinMode(12,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(500);// 单位是毫秒
+  digitalWrite(13,LOW);
+  delay(500);
+}
